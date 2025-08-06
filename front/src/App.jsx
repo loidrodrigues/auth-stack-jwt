5f1f3502448ec2ce66ddca9d6ab5a0a0ba81712a
Loidrodrigues <loidrodrigues5@gmail.com>
2025-08-06 12:09:59 -0300

    feat: criando a tela de login + estilizando

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,39 @@
-import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { register, handleSubmit } = useForm();
   return (
-    <>
-      <h1>Ola JWT - vamos praticar o fluxo de autenticacao</h1>
-    </>
+    <div className="h-screen bg-gradient-to-br from-slate-900 to-slate-600 text-white flex flex-col justify-center items-center gap-10">
+      <div className="flex flex-col items-center">
+        <h1 className="text-3xl font-bold">Auth with JWT</h1>
+        <span className="text-sm text-gray-500">Faça login para continuar</span>
+      </div>
+      <div className="bg-slate-800 p-8 rounded flex w-1/4">
+        <form className="flex flex-col gap-8 w-full">
+          <div className="flex flex-col gap-4">
+            <label htmlFor="email">E-mail</label>
+            <input
+              placeholder="Insira o seu email"
+              className="w-full border p-2 rounded"
+            />
+          </div>
+          <div className="flex flex-col gap-4">
+            <label htmlFor="email">Password</label>
+            <input
+              placeholder="Insira a sua senha"
+              className="w-full border p-2 rounded"
+            />
+          </div>
+          <button className="bg-rose-700 p-2 rounded text-white">Entrar</button>
+          <div className="flex gap-2 items-center">
+            <span className=" text-gray-500">Não possui uma conta?</span>
+            <a href="/register" className="text-white text-sm">
+              Cadastre-se
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
