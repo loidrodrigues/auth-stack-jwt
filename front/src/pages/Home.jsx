1950425fa07fe803546d901b00fe1f50c54fca2a
Loidrodrigues <loidrodrigues5@gmail.com>
2025-08-07 13:26:43 -0300

    feat: buscando dados do usuario

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,12 +1,33 @@
 export default function Home() {
+  const handleAccount = () => {
+    const token = localStorage.getItem("token");
+    fetch("http://localhost:3000/accounts", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        console.log(user);
+      });
+  };
   return (
     <div className="h-screen bg-gradient-to-br from-slate-900 to-slate-600">
       <div className="w-full bg-white flex justify-between gap-10 p-4  px-8">
         <h1 className="text-2xl font-bold italic">SUMSINE</h1>
         <div>
-          <ul className="flex gap-4">
+          <ul className="flex gap-4 items-center">
             <li className="font-semibold">Home</li>
-            <li className="font-semibold">Account</li>
+            <li className="font-semibold bg-slate-800 p-2 px-3 cursor-pointer text-white rounded">
+              <button
+                className="cursor-pointer font-light"
+                onClick={handleAccount}
+              >
+                Account
+              </button>
+            </li>
           </ul>
         </div>
       </div>
